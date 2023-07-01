79eea94deac32ab5babcfa6223ae749518502021
henryliem27 <henryliem27@gmail.com>
2023-07-01 12:39:03 +0700

    UI ButtonBack and navigationDrawer, sebelumnya error tidak ada button back

--- app/src/main/java/com/example/a160420048_uts_anmp/view/MainActivity.kt
+++ app/src/main/java/com/example/a160420048_uts_anmp/view/MainActivity.kt
@@ -3,75 +3,65 @@ package com.example.a160420048_uts_anmp.view
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.room.Room
-import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.navigation.ui.setupWithNavController
 import com.example.a160420048_uts_anmp.R
-import com.example.a160420048_uts_anmp.model.Doctor
-import com.example.a160420048_uts_anmp.model.DoctorDatabase
-import com.example.a160420048_uts_anmp.model.UTSDao
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.example.a160420048_uts_anmp.util.createNotificationChannel
-import com.google.android.material.internal.ContextUtils.getActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
-import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     @SuppressLint("MissingInflatedId")
     private val fragments:ArrayList<Fragment> =ArrayList()
     init {
         instance = this
     }
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_layout)
 
         createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
             getString(R.string.app_name), "App Notification Channel")
 
-//        val username = intent.getStringExtra(NAME)
-//        val bundle = Bundle()
-//        bundle.putString(NAME,username)
-
-        val bottomNav:BottomNavigationView = findViewById(R.id.bottomNav)
+        val username = intent.getStringExtra(NAME)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
         navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(bottomNav,navController)
+
 
         //toolbar button back sudah dicoding tapi tidak muncul
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        //navigation drawer
-        val drawerLayout = findViewById<View>(R.id.drawerLayout1)
-        var drawerToggle =
-            ActionBarDrawerToggle(this, drawerLayout as DrawerLayout?, toolbar,R.string.app_name,
-                R.string.app_name)
-        drawerToggle.isDrawerIndicatorEnabled = true
-        drawerToggle.syncState()
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        NavigationUI.setupWithNavController(toolbar,navController,appBarConfiguration)
+
+
+        //navigation drawer
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
         val navView = findViewById<NavigationView>(R.id.navView)
         NavigationUI.setupWithNavController(navView,navController)
 
-
+        //bottomNav
+        setSupportActionBar(toolbar)
+        val bottomNav:BottomNavigationView = findViewById(R.id.bottomNav)
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration)
+        NavigationUI.setupWithNavController(bottomNav,navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
